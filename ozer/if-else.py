792bf716bc07f867548ce5c91363c7bf4f386ff3
Mohlaroi <mohlaroisabirova13@gmail.com>
2023-10-19 19:25:51 +0300

    Initial commit

---
+++ ozer/if-else.py
@@ -0,0 +1,57 @@
+#old = int(input('Your age:'))
+#print('Recomended:', end='')
+#if 3 <=old<6:
+#    print('rabbit in maze')
+#elif 6<=old<12:
+#    print('Marsianin')
+#elif 12 <=old <16:
+#    print('mysterious island')
+#elif 16<= old:
+#    print('stream of consciousness')
+
+#a = 50
+#b = 100
+#c = 40
+#max = a if a > b else b
+#max = c if c > max else max
+#print(max)
+
+#a = 50
+#b = 100
+#c = 40
+#max = a if a < b else b
+#max = c if c < max else max
+#print(max)
+
+#c = 20
+#print('result = ', c + 20 if c >= 0 else c+1)
+#t = 10
+#while t:
+#    t -=1
+#    if t% 2 != 0: continue
+#    print(t, end='')
+
+#try:
+#    n = int(input('vvedite numder:'))
+#    print('luck')
+#except ValueError:
+#    print('mistake')
+
+#import math
+#n = input('vvedite integer number:')
+#while type(n) != int:
+#    try:
+#        n = int(n)
+#    except ValueError:
+#        print('incorrectly entered')
+#        n = input('ENTER IN INTEGER:')
+#if not(math.fmod(n,2)):
+#    print('even')
+#else:
+#    print('odd')
+
+
+def CharS():
+    print('S', end='')
+def CharO():
+    print()
